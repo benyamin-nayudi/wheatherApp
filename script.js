3f44a4adf9186a279ddb083f0098d578ef8993e9
benyamin-nayudi <benyamin.naudi@gmail.com>
2021-10-18 18:40:12 +0330

    making cards

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
-// api.openweathermap.org/data/2.5/weather?q={city name}&appid=04e6727b1951a0214305fe9bde8394e4
+// `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=04e6727b1951a0214305fe9bde8394e4`
+const container = document.querySelector('#cardContainer')
 
 const form = document.querySelector('form')
-form.addEventListener('submit' , (e)=>{
+form.addEventListener('submit', (e) => {
     e.preventDefault();
+
     const city = form.city.value;
     console.log(city)
-    // getWeater(city)
-    fetch(`api.openweathermap.org/data/2.5/weather?q=miami&appid=04e6727b1951a0214305fe9bde8394e4`)
-    .then(res => res.json())
-    .then(data => console.log(data))
+    getWeather(city).then(res => {
+        const icon = <i class="fas fa-cloud-moon"></i>
+
+        console.log(res);
+        container.innerHTML = `
+         <div class="card mx-auto mt-4 cardDiv" style="width: 18rem;">
+                <img src="img/cloudy-day.jpg" class="card-img-top" alt="weather">
+                <div class="card-body cardBody">
+                    <div class="font-awesome">
+                        <i class="fas fa-cloud-moon"></i>
+                    </div>
+                    <div class="cardText pt-3">
+                        <p class="city-name">MIAMI</p>
+                        <p class="weather">clear</p>
+                        <p class="degree">15.4 C</p>
+                    </div>
+                </div>
+            </div>
+         `
+    })
 
 })
 
-const getWeater = async(city) => {
-    // try{
-    //     const weatherApi = await fetch(`api.openweathermap.org/data/2.5/weather?q=${city}&appid=04e6727b1951a0214305fe9bde8394e4`)
-    //     const data = await weatherApi.json();
-    //     console.log(data)
-    // } catch(err){
-    //     console.log(err)
-    // }
+const getWeather = async (city) => {
+    try {
+        const weatherApi = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=04e6727b1951a0214305fe9bde8394e4`)
+        const data = await weatherApi.json();
+        return data
+    } catch (err) {
+        console.log(err)
+    }
 
-    
+    // fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=04e6727b1951a0214305fe9bde8394e4`)
+    //     .then(res => res.json())
+    //     .then(data => console.log(data))
 }
